2c3d51509d05315b676209186218e021bc3e5ff9
anoushk1234 <anoushka1828@gmail.com>
2021-12-25 16:08:40 +0530

    font plus ts error

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  ignoreBuildErrors: true,
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
-import type { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/provider'
-import theme from '../utils/theme'
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/provider";
+import theme from "../utils/theme";
+import "@fontsource/lexend/latin.css";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-  <ChakraProvider theme={theme}>
-  <Component {...pageProps} />
-  </ChakraProvider>)
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
