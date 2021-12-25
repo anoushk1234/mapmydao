f00378895e508891e7ae24b9791d8e54935a9edc
anoushk1234 <anoushka1828@gmail.com>
2021-12-25 14:12:14 +0530

    some more

--- pages/dash.tsx
+++ pages/dash.tsx
@@ -18,6 +18,7 @@ import {
   Tr,
   Th,
   Td,
+  Select,
   Divider,
   Link,
   Box,
@@ -48,13 +49,50 @@ const Dash: NextPage = () => {
   const [value, changeValue] = useState(1);
   const [tab, setTab] = useState("map");
   const [user, setUser] = useState({});
+  const [daoList, setDaoList] = useState([]);
+  const [dao, setDao] = useState({});
+  const [daoMembers, setDaoMembers] = useState([]);
   const router = useRouter();
 
   useEffect(() => {
     const user2 = supabase.auth.user();
+    console.log(user2);
     setUser(user2 != null ? user2 : {});
-  }, []);
+  });
 
+  useEffect(() => {
+    async function getUserDaosFromSupabase() {
+      const { data, error } = await supabase
+        .from("daos")
+        .select()
+        .eq(
+          "signer_id",
+          user.user_metadata != undefined ? user.user_metadata.provider_id : ""
+        );
+      if (error) {
+        console.log(error);
+      }
+      console.log(data.length > 0 ? data[0].uid : {});
+      data.length > 0 ? setDaoList(data) : null;
+    }
+    getUserDaosFromSupabase();
+  }, [user]);
+
+  useEffect(() => {
+    async function getDaoMembersFromSupabase() {
+      // dao ? console.log(dao) : console.log("no dao");
+      const { data, error } = await supabase
+        .from("users")
+        .select()
+        .eq("dao", dao);
+      if (error) {
+        console.log(error);
+      }
+      console.log(data);
+      setDaoMembers(data as any);
+    }
+    dao ? getDaoMembersFromSupabase() : null;
+  }, [dao, user]);
   useEffect(() => {
     const map = new mapboxgl.Map({
       container: mapContainerRef.current || "map",
@@ -398,247 +436,47 @@ const Dash: NextPage = () => {
                 />
                 <Input type="number" placeholder="Search" borderRadius="10px" />
               </InputGroup>
-              <IconButton
-                aria-label="expand"
-                icon={<FiBell />}
-                fontSize="sm"
-                bgColor="#fff"
-                borderRadius="50%"
-                p="10px"
-              />
-              <Flex
-                w={30}
-                h={25}
-                bgColor="#B57295"
-                borderRadius="50%"
-                color="#fff"
-                align="center"
-                justify="center"
-                ml="-3"
-                mt="-2"
-                zIndex="100"
-                fontSize="xs"
-              >
-                2
-              </Flex>
             </Flex>
             <Heading letterSpacing="tight"></Heading>
-            {/* {value == 1 && (
-            <Box
-              borderRadius="25px"
-              mt={4}
-              w="100%"
-              h="200px"
-              bgGradient="linear(to-t, #B57295, #29259A)"
-            >
-              <Flex
-                p="1em"
-                color="#fff"
-                flexDir="column"
-                h="100%"
-                justify="space-between"
-              >
-                <Flex justify="space-between" w="100%" align="flex-start">
-                  <Flex flexDir="column">
-                    <Text color="gray.400">Current Balance</Text>
-                    <Text fontWeight="bold" fontSize="xl">
-                      $5,750.20
-                    </Text>
-                  </Flex>
-                  <Flex align="center">
-                    <Icon mr={2} as={FiCreditCard} />
-                    <Text>Rise.</Text>
-                  </Flex>
-                </Flex>
-                <Text mb={4}>**** **** **** 1289</Text>
-                <Flex align="flex-end" justify="space-between">
-                  <Flex>
-                    <Flex flexDir="column" mr={4}>
-                      <Text textTransform="uppercase" fontSize="xs">
-                        Valid Thru
-                      </Text>
-                      <Text fontSize="lg">12/23</Text>
-                    </Flex>
-                    <Flex flexDir="column">
-                      <Text textTransform="uppercase" fontSize="xs">
-                        CVV
-                      </Text>
-                      <Text fontSize="lg">***</Text>
-                    </Flex>
-                  </Flex>
-                  <Icon as={FiCreditCard} />
-                </Flex>
-              </Flex>
-            </Box>
-          )} */}
-            {/* {value == 2 && (
-            <Box
-              borderRadius="25px"
-              mt={4}
-              w="100%"
-              h="200px"
-              bgGradient="linear(to-t, yellow.300, blue.500)"
-            >
-              <Flex
-                p="1em"
-                color="#fff"
-                flexDir="column"
-                h="100%"
-                justify="space-between"
-              >
-                <Flex justify="space-between" w="100%" align="flex-start">
-                  <Flex flexDir="column">
-                    <Text color="gray.400">Current Balance</Text>
-                    <Text fontWeight="bold" fontSize="xl">
-                      $350.00
-                    </Text>
-                  </Flex>
-                  <Flex align="center">
-                    <Icon mr={2} as={FiCreditCard} />
-                    <Text>Rise.</Text>
-                  </Flex>
-                </Flex>
-                <Text mb={4}>**** **** **** 8956</Text>
-                <Flex align="flex-end" justify="space-between">
-                  <Flex>
-                    <Flex flexDir="column" mr={4}>
-                      <Text textTransform="uppercase" fontSize="xs">
-                        Valid Thru
-                      </Text>
-                      <Text fontSize="lg">9/24</Text>
-                    </Flex>
-                    <Flex flexDir="column">
-                      <Text textTransform="uppercase" fontSize="xs">
-                        CVV
-                      </Text>
-                      <Text fontSize="lg">***</Text>
-                    </Flex>
-                  </Flex>
-                  <Icon as={FiCreditCard} />
-                </Flex>
-              </Flex>
-            </Box>
-          )} */}
-            {/* {value == 3 && (
-            <Box
-              borderRadius="25px"
-              mt={4}
-              w="100%"
-              h="200px"
-              bgGradient="linear(to-t, orange.300, pink.600)"
-            >
-              <Flex
-                p="1em"
-                color="#fff"
-                flexDir="column"
-                h="100%"
-                justify="space-between"
-              >
-                <Flex justify="space-between" w="100%" align="flex-start">
-                  <Flex flexDir="column">
-                    <Text color="gray.400">Current Balance</Text>
-                    <Text fontWeight="bold" fontSize="xl">
-                      $2,150.72
-                    </Text>
-                  </Flex>
-                  <Flex align="center">
-                    <Icon mr={2} as={FiCreditCard} />
-                    <Text>Rise.</Text>
-                  </Flex>
-                </Flex>
-                <Text mb={4}>**** **** **** 8353</Text>
-                <Flex align="flex-end" justify="space-between">
-                  <Flex>
-                    <Flex flexDir="column" mr={4}>
-                      <Text textTransform="uppercase" fontSize="xs">
-                        Valid Thru
-                      </Text>
-                      <Text fontSize="lg">11/22</Text>
-                    </Flex>
-                    <Flex flexDir="column">
-                      <Text textTransform="uppercase" fontSize="xs">
-                        CVV
-                      </Text>
-                      <Text fontSize="lg">***</Text>
-                    </Flex>
-                  </Flex>
-                  <Icon as={FiCreditCard} />
-                </Flex>
-              </Flex>
-            </Box>
-          )} */}
-            {/* <Flex justifyContent="center" mt={2}>
-            <Button
-              bgColor={value == 1 ? "gray.600" : "gray.400"}
-              size="xs"
-              mx={1}
-              onClick={() => changeValue(1)}
-            />
-            <Button
-              bgColor={value == 2 ? "gray.600" : "gray.400"}
-              size="xs"
-              mx={1}
-              onClick={() => changeValue(2)}
-            />
-            <Button
-              bgColor={value == 3 ? "gray.600" : "gray.400"}
-              size="xs"
-              mx={1}
-              onClick={() => changeValue(3)}
-            />
-          </Flex> */}
-            {/* <Flex flexDir="column" my={4}>
-            <Flex justify="space-between" mb={2}>
-              <Text>Balance</Text>
-              <Text fontWeight="bold">$140.42</Text>
-            </Flex>
-            <Flex justify="space-between">
-              <Text>Credit Limit</Text>
-              <Text fontWeight="bold">$150.00</Text>
-            </Flex>
-          </Flex> */}
-            {/* <Heading letterSpacing="tight" size="md" my={4}>
-            Send money to
-          </Heading>
-          <Flex>
-            <AvatarGroup size="md" max={3}>
-              <Avatar src="avatar-2.jpg" />
-              <Avatar src="avatar-3.jpg" />
-              <Avatar src="avatar-4.jpg" />
-              <Avatar src="avatar-4.jpg" />
-              <Avatar src="avatar-4.jpg" />
-            </AvatarGroup>
-            <Avatar icon={<FiPlus />} ml={2} color="#fff" bgColor="gray.300" />
-          </Flex>
-          <Text color="gray" mt={10} mb={2}>
-            Card number
-          </Text>
-          <InputGroup>
-            <InputLeftElement
-              pointerEvents="none"
-              children={<FiCreditCard color="gray.700" />}
-            />
-            <Input type="number" placeholder="xxxx xxxx xxxx xxxx" />
-          </InputGroup>
-          <Text color="gray" mt={4} mb={2}>
-            Sum
-          </Text>
-          <InputGroup>
-            <InputLeftElement
-              pointerEvents="none"
-              children={<FiDollarSign color="gray.700" />}
-            />
-            <Input type="number" placeholder="130.00" />
-          </InputGroup> */}
+
+            <Heading letterSpacing="tight" size="md" my={4}>
+              Setup your Dao
+            </Heading>
             <Button
               mt={4}
               bgColor="blackAlpha.900"
               color="#fff"
               p={7}
               borderRadius={15}
+              onClick={() => {
+                window.open(process.env.NEXT_PUBLIC_BOT_URL, "_blank");
+              }}
             >
-              Send money
+              Add the bot
             </Button>
+            <Text color="gray" mt={4} mb={2}>
+              Use the ~config command to register your Dao
+            </Text>
+            <Select
+              placeholder="Select a Dao"
+              onChange={(e) => {
+                // console.log(typeof e.target.value);
+                // console.log(e.target.value);
+                setDao(e.target.value);
+              }}
+            >
+              {/* {dao.length > 1 ? console.log(dao) : null} */}
+
+              {daoList.length > 0 ? (
+                daoList.map((daoitem, index) => (
+                  <option key={index} value={daoitem.uid}>
+                    {daoitem.name}
+                  </option>
+                ))
+              ) : (
+                <></>
+              )}
+            </Select>
           </Flex>
         </Flex>
       }
